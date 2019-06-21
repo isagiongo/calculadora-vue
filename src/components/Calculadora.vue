2413b71370be816a23dc483c7c1172c0fca094cb
Isa Giongo <isagiongo@gmail.com>
2019-06-21 10:59:24 -0300

    Aprimora css da calculadora

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculadora">
     <div class="display">{{valorCorrente || '0'}}</div>
-    <div v-on:click="limpar" class="botao">C</div>
-    <div v-on:click="sinal" class="botao">+/-</div>
-    <div v-on:click="porcentagem" class="botao">%</div>
-    <div v-on:click="dividir" class="botao operadores">/</div>
-    <div v-on:click="juntarNumeros('7')" class="botao">7</div>
-    <div v-on:click="juntarNumeros('8')" class="botao">8</div>
-    <div v-on:click="juntarNumeros('9')" class="botao">9</div>
-    <div v-on:click="multiplicar" class="botao operadores">x</div>
-    <div v-on:click="juntarNumeros('4')" class="botao">4</div>
-    <div v-on:click="juntarNumeros('5')" class="botao">5</div>
-    <div v-on:click="juntarNumeros('6')" class="botao">6</div>
-    <div v-on:click="subtrair" class="botao operadores">-</div>
-    <div v-on:click="juntarNumeros('1')" class="botao">1</div>
-    <div v-on:click="juntarNumeros('2')" class="botao">2</div>
-    <div v-on:click="juntarNumeros('3')" class="botao">3</div>
-    <div v-on:click="somar" class="botao operadores">+</div>
-    <div v-on:click="juntarNumeros('0')" class="botao zero">0</div>
-    <div v-on:click="ponto" class="botao">.</div>
-    <div v-on:click="resultado" class="botao operadores">=</div>
+    <div v-on:click="limpar" class="botao"><span>C</span></div>
+    <div v-on:click="sinal" class="botao"><span>+/-</span></div>
+    <div v-on:click="porcentagem" class="botao"><span>%</span></div>
+    <div v-on:click="dividir" class="botao operadores"><span>/</span></div>
+    <div v-on:click="juntarNumeros('7')" class="botao"><span>7</span></div>
+    <div v-on:click="juntarNumeros('8')" class="botao"><span>8</span></div>
+    <div v-on:click="juntarNumeros('9')" class="botao"><span>9</span></div>
+    <div v-on:click="multiplicar" class="botao operadores"><span>x</span></div>
+    <div v-on:click="juntarNumeros('4')" class="botao"><span>4</span></div>
+    <div v-on:click="juntarNumeros('5')" class="botao"><span>5</span></div>
+    <div v-on:click="juntarNumeros('6')" class="botao"><span>6</span></div>
+    <div v-on:click="subtrair" class="botao operadores"><span>-</span></div>
+    <div v-on:click="juntarNumeros('1')" class="botao"><span>1</span></div>
+    <div v-on:click="juntarNumeros('2')" class="botao"><span>2</span></div>
+    <div v-on:click="juntarNumeros('3')" class="botao"><span>3</span></div>
+    <div v-on:click="somar" class="botao operadores"><span>+</span></div>
+    <div v-on:click="juntarNumeros('0')" class="botao zero"><span>0</span></div>
+    <div v-on:click="ponto" class="botao"><span>.</span></div>
+    <div v-on:click="resultado" class="botao operadores"><span>=</span></div>
   </div>
 </template>
 
@@ -109,7 +109,35 @@ export default {
 .botao {
   background-color: #f2f2f2;
   border: 1px solid #999;
+  transition: all 0.5s;
+  cursor: pointer;
 }
+
+.botao span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.botao span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.botao:hover span {
+  padding-right: 25px;
+}
+
+.botao:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
 .operadores {
   background-color: orange;
   color: white;
