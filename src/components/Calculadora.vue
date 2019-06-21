38385ffab5b86ec54876cdee7137200c888d7ded
Isa Giongo <isagiongo@gmail.com>
2019-06-21 09:50:54 -0300

    Implementa botoes de operacoes matematicas

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -2,24 +2,24 @@
   <div class="calculadora">
     <div class="display">{{valorCorrente || '0'}}</div>
     <div v-on:click="limpar" class="botao">C</div>
-    <div v-on:click="limpar" class="botao">+/-</div>
-    <div v-on:click="limpar" class="botao">%</div>
-    <div class="botao operadores">/</div>
-    <div class="botao">7</div>
-    <div class="botao">8</div>
-    <div class="botao">9</div>
-    <div class="botao operadores">x</div>
-    <div class="botao">4</div>
-    <div class="botao">5</div>
-    <div class="botao">6</div>
-    <div class="botao operadores">-</div>
-    <div class="botao">1</div>
-    <div class="botao">2</div>
-    <div class="botao">3</div>
-    <div class="botao operadores">+</div>
-    <div class="botao zero">0</div>
-    <div class="botao">.</div>
-    <div class="botao">=</div>
+    <div v-on:click="sinal" class="botao">+/-</div>
+    <div v-on:click="porcentagem" class="botao">%</div>
+    <div v-on:click="dividir" class="botao operadores">/</div>
+    <div v-on:click="juntarNumeros('7')" class="botao">7</div>
+    <div v-on:click="juntarNumeros('8')" class="botao">8</div>
+    <div v-on:click="juntarNumeros('9')" class="botao">9</div>
+    <div v-on:click="multiplicar" class="botao operadores">x</div>
+    <div v-on:click="juntarNumeros('4')" class="botao">4</div>
+    <div v-on:click="juntarNumeros('5')" class="botao">5</div>
+    <div v-on:click="juntarNumeros('6')" class="botao">6</div>
+    <div v-on:click="subtrair" class="botao operadores">-</div>
+    <div v-on:click="juntarNumeros('1')" class="botao">1</div>
+    <div v-on:click="juntarNumeros('2')" class="botao">2</div>
+    <div v-on:click="juntarNumeros('3')" class="botao">3</div>
+    <div v-on:click="somar" class="botao operadores">+</div>
+    <div v-on:click="juntarNumeros('0')" class="botao zero">0</div>
+    <div v-on:click="ponto" class="botao">.</div>
+    <div v-on:click="resultado" class="botao operadores">=</div>
   </div>
 </template>
 
@@ -27,8 +27,63 @@
 export default {
   data() {
     return {
-      valorCorrente: 123,
+      valorCorrente: '',
+      numeroAnterior: null,
+      operador: null,
+      operadorClicado: false,
     };
+  },
+  methods: {
+    limpar() {
+      this.valorCorrente = '';
+    },
+    sinal() {
+      this.valorCorrente = this.valorCorrente.charAt(0) === '-'
+        ? this.valorCorrente.slice(1)
+        : `-${this.valorCorrente}`;
+    },
+    porcentagem() {
+      this.valorCorrente = `${parseFloat(this.valorCorrente) / 100}`;
+    },
+    juntarNumeros(numero) {
+      if (this.operadorClicado) {
+        this.valorCorrente = '';
+        this.operadorClicado = false;
+      }
+      this.valorCorrente = `${this.valorCorrente}${numero}`;
+    },
+    ponto() {
+      if (this.valorCorrente.indexOf('.') === -1) {
+        this.juntarNumeros('.');
+      }
+    },
+    setarValor() {
+      this.numeroAnterior = this.valorCorrente;
+      this.operadorClicado = true;
+    },
+    dividir() {
+      this.operador = (num1, num2) => num1 / num2;
+      this.setarValor();
+    },
+    multiplicar() {
+      this.operador = (num1, num2) => num1 * num2;
+      this.setarValor();
+    },
+    somar() {
+      this.operador = (num1, num2) => num1 + num2;
+      this.setarValor();
+    },
+    subtrair() {
+      this.operador = (num1, num2) => num1 - num2;
+      this.setarValor();
+    },
+    resultado() {
+      this.valorCorrente = `${this.operador(
+        parseFloat(this.numeroAnterior),
+        parseFloat(this.valorCorrente),
+      )}`;
+      this.numeroAnterior = null;
+    },
   },
 };
 </script>
